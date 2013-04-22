187ccc63feef3b0025cf97cb357ae47e9ca9e368
makto <zhongqi90@gmail.com>
2013-04-22 12:29:43 +0800

    show bytes of c data objects

---
+++ show_bytes.c
@@ -0,0 +1,43 @@
+/* CSAPP 28页 示例程序
+ * code/data/show-bytes.c
+ * 用来可视化数据对象的字节表示 */
+
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len){
+    int i;
+    for(i=0; i<len; i++)
+        printf(" %.2x", start[i]);
+    printf("\n");
+}
+
+void show_int(int x){
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x){
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x){
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+void test_show_bytes(int val){
+    int ival = val;
+    float fval = (float) ival;
+    int *pval = &ival;
+    show_int(ival);
+    show_float(fval);
+    show_pointer(pval);
+    byte_pointer valp = (byte_pointer) &val;
+    show_bytes(valp, 1);
+    show_bytes(valp, 2);
+}
+
+int main(){
+    test_show_bytes(12345);
+    return 0;
+}
